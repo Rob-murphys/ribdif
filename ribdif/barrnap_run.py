fa662128fe45529ccb9a64f6714eb90361e22cf4
Rob Murphy <49655395+Rob-murphys@users.noreply.github.com>
2023-01-09 16:22:00 +0100

    Get and split 16S sequences from barrnap

--- ribdif/barrnap_run.py
+++ ribdif/barrnap_run.py
@@ -20,16 +20,22 @@ def barnap_call(outdir):
     print("\n\nRunning barrnap on downloaded sequences")
     Ncpu = multiprocessing.cpu_count()
     with multiprocessing.Pool(Ncpu) as pool: # spawn the pool
-        all_fna = [str(i) for i in list(Path(f"{outdir}/genbank/bacteria/").rglob('*.fna'))]
-        pool.map(call_proc, all_fna)
+        all_fna = [str(i) for i in list(Path(f"{outdir}/genbank/bacteria/").rglob('*.fna'))] # generate list of files ending in .rna
+        pool.map(call_proc, all_fna) # 
     #pool.join() # The pool will close and wait for each running task to complete
 
-def barrnap_get(RNA):
-    with open(RNA, "r") as f_in, open(f"{RNA}.16S", "w") as f_out:
+def barrnap_split(RNA):
+    count = 0
+    dir_16S = (Path(RNA).parent / "indiv_16S_dir") # directory path for individual 16S files
+    dir_16S.mkdir(exist_ok=True, parents=True) # create directory
+    with open(RNA, "r") as f_in: 
         for line in f_in:
             if line.startswith(">16S_rRNA"):
-                f_out.write(line.strip())
-                f_out.write(next(line.strip()))
+                with open(f"{dir_16S}/{count}.fna", "w") as f_out:
+                    f_out.write(">" + line.strip().strip(">16S_rRNA::") + f"_{count}\n") # write the fasta header to file removing >16S_rRNA:: and adding a counter
+                    f_out.write(next(f_in)) # write next line also
+                count += 1 # incriment count by one
+
                 
     
 
