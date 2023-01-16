ef756141a6efcbbfd563558757f49c77706ff215
Rob Murphy <49655395+Rob-murphys@users.noreply.github.com>
2023-01-16 15:12:22 +0100

    Fixing outdir and adding reporting

--- ribdif/vsearch_run.py
+++ ribdif/vsearch_run.py
@@ -5,17 +5,18 @@ import shlex
 from pathlib import Path
 
 
-def vsearch_call(outdir, genus, name):
+def vsearch_call(outdir, genus, name, ident, log_dir):
     # Defining in and out files/dirs
     infile = f"{outdir}/amplicons/{genus}-{name}.amplicons"
     outfile = f"{outdir}/amplicons/{genus}-{name}.uc"
-    outdir = f"{outdir}/amplicons/{name}-clusters/{genus}-{name}-clus"
+    clusdir = f"{outdir}/amplicons/{name}-clusters/{genus}-{name}-clus"
     
-    Path.mkdir(Path(outdir), exist_ok = True, parents = True) # making outdir
+    Path.mkdir(Path(clusdir).parent, exist_ok = True, parents = True) # making outdir
     
     # Building the command
-    command = "vsearch -cluster_fast {infile} --id $id  -strand both --uc {outfile} --clusters {outdir} --quiet"
+    command = "vsearch --cluster_fast {infile} --id {ident}  -strand both --uc {outfile} --clusters {clusdir} --quiet"
     
-    # Passing the command to shell piping the stdout and stderr
-    subprocess.run(shlex.split(command), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    with open(f"{log_dir}/vsearch.out", "w") as out, open(f"{log_dir}/vsearch.err", "w") as err:
+        # Passing the command to shell piping the stdout and stderr
+        subprocess.run(shlex.split(command), stdout = out, stderr = err)
     return
