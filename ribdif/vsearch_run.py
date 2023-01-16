c005dfac186715d5d52f78d40262ed50c3458ec8
Rob Murphy <49655395+Rob-murphys@users.noreply.github.com>
2023-01-16 15:33:03 +0100

    Catch is Vsearch fails

--- ribdif/vsearch_run.py
+++ ribdif/vsearch_run.py
@@ -3,6 +3,8 @@
 import subprocess
 import shlex
 from pathlib import Path
+import os
+
 
 
 def vsearch_call(outdir, genus, name, ident, log_dir):
@@ -14,9 +16,11 @@ def vsearch_call(outdir, genus, name, ident, log_dir):
     Path.mkdir(Path(clusdir).parent, exist_ok = True, parents = True) # making outdir
     
     # Building the command
-    command = "vsearch --cluster_fast {infile} --id {ident}  -strand both --uc {outfile} --clusters {clusdir} --quiet"
-    
-    with open(f"{log_dir}/vsearch.out", "w") as out, open(f"{log_dir}/vsearch.err", "w") as err:
+    command = f"vsearch --cluster_fast {infile} --id {ident} --strand both --uc {outfile} --clusters {clusdir} --quiet"
+    with open(f"{log_dir}/vsearch_{name}.out", "w") as out, open(f"{log_dir}/vsearch_{name}.err", "w") as err:
         # Passing the command to shell piping the stdout and stderr
         subprocess.run(shlex.split(command), stdout = out, stderr = err)
+    
+    if os.stat(err).st_size == 0:
+        raise Exception(f"Something went wrong with VSEARCH\n\nPlease check: {err}")
     return
