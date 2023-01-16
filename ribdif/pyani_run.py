a2b0f7b6b36dbca09ce6b32e8ba6b9a344b6cd4f
Rob Murphy <49655395+Rob-murphys@users.noreply.github.com>
2023-01-16 14:06:55 +0100

    Adding threads argument

--- ribdif/pyani_run.py
+++ ribdif/pyani_run.py
@@ -16,9 +16,8 @@ def call_proc_pyani(indir):
 
 
 # Multithreading the pyani calls
-def pyani_call(outdir):
-    Ncpu = multiprocessing.cpu_count()
-    with multiprocessing.Pool(Ncpu) as pool: # spawn the pool
+def pyani_call(outdir, threads):
+    with multiprocessing.Pool(threads) as pool: # spawn the pool
         all_16S_dirs = [i for i in glob(f"{outdir}/genbank/bacteria/*/indiv_16S_dir")] # can we use a faster method than glob?
         pool.map(call_proc_pyani, all_16S_dirs)
     return
