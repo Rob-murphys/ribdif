89f03aa1863bd057e47bace35f43ce9f3f9fe6ce
Rob Murphy <49655395+Rob-murphys@users.noreply.github.com>
2023-01-17 16:25:37 +0100

    Added format trees function

--- ribdif/msa_run.py
+++ ribdif/msa_run.py
@@ -4,6 +4,10 @@ import multiprocessing
 from pathlib import Path
 import subprocess
 import shlex
+from Bio import Phylo
+import pandas as pd
+import re
+
 
 # Spawning the shell call
 def call_proc_muscle(infile):
@@ -38,3 +42,35 @@ def muscle_call_single(infile, outAln, outTree):
         subprocess.run(shlex.split(command2), stdout = f_std, stderr = subprocess.PIPE)
     return
 
+def format_trees(outdir, genus, name):
+    uc_path = f"{outdir}/amplicons/{genus}-{name}.uc"
+    tree_path = f"{outdir}/amplicons/{genus}-{name}.tree"
+    out_path = f"{outdir}/amplicons/{genus}-{name}-meta.tsv"
+    
+    # read in cluster file
+    uc_df = pd.read_csv(uc_path, sep = "\t", header = None)
+    uc_df_clean = uc_df.loc[uc_df[0] != "C"]
+    
+    # read in tree and get lead labels
+    tree = Phylo.read(tree_path, "newick")
+    tree_terminals = tree.get_terminals() # getting all leaf info
+    tips = [t.name for t in tree_terminals] # getting just leaf names
+    tips_clean = [re.sub(r"_chromosome_.*|_complete_.*|_genome_.*", "", n) for n in tips] # cleaning leaf names
+
+    
+    # Getting individual name parts with list comprehension
+    GCF = ["_".join(i.split("_")[:2]) for i in tips_clean]
+    NZ = [i.split("_")[2] for i in tips_clean]
+    species = [i.split("_")[4] for i in tips_clean]
+    strain = ["_".join(i.split("_")[5:])for i in tips_clean]
+    n_gene = [i.split("_")[-1] for i in tips]
+    
+    # Getting cluster number for each item of tips in the order of tips # Why not just reorder tips to that of the dataframe?
+    uc_df = uc_df.sort_values(by = tips)
+    match_index =[uc_df_clean.index[uc_df_clean[8] == i] for i in tips]
+    clusters = [uc_df.loc[m][1].values[0] for m in match_index]
+    
+    meta_df = pd.DataFrame({"name": tips, "GCF":GCF, "NZ":NZ, "Species":species, "Strain":strain, "nGene":n_gene, "Cluster":clusters})
+    meta_df.to_csv(out_path, sep = "\t", index = False)
+    
+  
