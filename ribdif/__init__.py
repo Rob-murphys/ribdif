7b4a65df6e53ce080c36f5f5cd8f6b5ba822bb41
Rob Murphy <49655395+Rob-murphys@users.noreply.github.com>
2023-02-07 16:23:29 +0100

    Update __init__.py

--- ribdif/__init__.py
+++ ribdif/__init__.py
@@ -9,14 +9,10 @@ RibDif2 contains the following moduels:
 """
 __authors__ = "Robert Murphy"
 __licence__ = "GPL-3.0"
-__version__ = (2, 0, 0)
+__version__ = (1, 1, 1)
 
 
 # Try on python v3.5
 import sys as _sys
-if _sys.version_info[:2] < (3, 5):
-    raise ImportError('Python version must be >= 3.5')
-    
-
-from . import ngd_download
-from . import barrnap_run
+if _sys.version_info[:2] < (3, 7):
+    raise ImportError('Python version must be >= 3.7')
