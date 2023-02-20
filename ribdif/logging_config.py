b7975b864a5eb5bb3d0d91bd11c95c03d5003dbb
Rob Murphy <49655395+Rob-murphys@users.noreply.github.com>
2023-02-20 15:23:10 +0100

    changing config

--- ribdif/logging_config.py
+++ ribdif/logging_config.py
@@ -12,14 +12,14 @@ def cofigure_logging():
     
     # create console handler with filter
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.ERROR)
-    console_handler.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.Formatter('%(message)s'))
     console_handler.addFilter(TracebackFilter())
     logger.addHandler(console_handler)
     
     # create file handler without filter
     file_handler = logging.FileHandler("tmpfile.log", "w")
-    file_handler.setLevel(logging.DEBUG)
+    file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
     logger.addHandler(file_handler)
     return logger
@@ -33,7 +33,7 @@ def replace_log_file(outdir, logger):
     
     with open(logger.handlers[1].baseFilename, "r") as log_in, open(logger.handlers[2].baseFilename, "a") as log_out:
         log_out.write(log_in.read())
-    logger.removeHandler(logger.handlers[0])
+    logger.removeHandler(logger.handlers[1])
 
     return
 
