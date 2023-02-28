82ed392f7f9f5fbb83f5f63a3a697fc985bd9e3b
Rob Murphy <49655395+Rob-murphys@users.noreply.github.com>
2023-02-28 12:01:03 +0100

    adding buffer to max length for wg mode pcr

--- ribdif/pcr_run.py
+++ ribdif/pcr_run.py
@@ -46,7 +46,8 @@ def pcr_parallel_call(outdir, genus, primer_file, workingDir, threads, logger, d
             with multiprocessing.Pool(threads) as pool: # spawn the pool # opening the pool
                 all_fna = [str(i) for i in list(Path(f"{outdir}/refseq/{domain}/").rglob('*.fna'))] # generate list of files ending in .fna
                 #counter = range(len(all_fna))
-                pool.starmap(call_proc_pcr, zip(all_fna, repeat(outdir), repeat(genus), repeat(name), repeat(fwd), repeat(rvs), repeat(length), repeat(workingDir), repeat(multi))) # removed counter
+                longer_length = int((float(length)+(float(length)*0.2)))
+                pool.starmap(call_proc_pcr, zip(all_fna, repeat(outdir), repeat(genus), repeat(name), repeat(fwd), repeat(rvs), repeat(longer_length), repeat(workingDir), repeat(multi))) # removed counter
             names.append(name)
     return names
 
