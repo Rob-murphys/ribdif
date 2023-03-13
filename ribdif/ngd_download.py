cc5182c5dce8ac817a4513ec567a7743dfc7529d
Rob Murphy <49655395+Rob-murphys@users.noreply.github.com>
2023-03-13 13:48:32 +0100

    Reformat code and add retry for non bacteria domain

--- ribdif/ngd_download.py
+++ ribdif/ngd_download.py
@@ -6,55 +6,69 @@ import shutil
 import logging
 # Using Kai's NCBI genome downloader to get all genones of the specified genus
 # Avaliable at: https://github.com/kblin/ncbi-genome-download
-def genome_download(genus, outdir, threads, frag, sp_ignore, domain, logger):
-    genera = genus.replace("_", " ")
-    if frag: # if the user wants fragmented genomes
-        logger.info(f"Downloading all genome recoreds of {genus} fron NCBI\n")
-        ngd.download(section='refseq', 
-               file_formats = 'fasta', 
-               genera = genera,  
-               output = outdir, 
-               parallel = threads*2,
-               groups = domain)
-        
-        
-    else: # if the user (correctly) only want complete genomes
-        logger.info(f"Downloading all complete genome records of {genus} from NCBI\n")
-        ngd.download(section='refseq', 
-               file_formats = 'fasta', 
-               genera = genera,  
-               output = outdir, 
-               assembly_levels = "complete",
-               parallel = threads*2,
-               groups = domain)
-      
-    try:
-        if Path(f"{outdir}/refseq/{domain}").is_dir():
+
+def genome_download(genus, outdir, threads, frag, sp_ignore, domain, logger, assembly_level):
+    genera = genus.replace("_", " ") # replace "_" with " "
+    assembly_level = "all" if frag else "complete" # assign assembly level based on user input
     
-            count = len(list(Path(f"{outdir}/refseq/{domain}").glob("*/*.fna.gz")))
-            dir_count = len(list(Path(f"{outdir}/refseq/{domain}").glob("*")))
+    # Download genomes with specific domain and genus/species
+    logger.info(f"Downloading all genome records of {genus} from NCBI at {assembly_level} assembly level\n")
+    ngd.download(section='refseq', 
+           file_formats = 'fasta', 
+           genera = genera,  
+           output = outdir, 
+           assembly_levels = assembly_level,
+           parallel = threads*2,
+           groups = domain)
+    status, count = download_checker(outdir, domain, logger, genus, sp_ignore)
+    
+    # if non bacteria domain and no complete genomes were downloaded, try again at chromosome level as it is very rare to have "complete" non bacteria genomes
+    if domain != "bacteria" and status == 1 and assembly_level == "complete":
+        logger.info(f"No complete genomes for {genus} were found so we will try again using 'chromosome' assembly level as this is what non bacteria genomes are usually added as")
+        assembly_level = "chromosome"
+        ngd.download(section='refseq', 
+               file_formats = 'fasta', 
+               genera = genera,  
+               output = outdir, 
+               assembly_levels = assembly_level,
+               parallel = threads*2,
+               groups = domain)
+        
+    # Remove genomes of unknown species if genomes were downloaded (status ==0)
+    if sp_ignore and status == 0:
+        sp_count = sp_remove(outdir, domain) # Remove genomes of unknown species
+        logger.info(f"{count} genomes of {genus} were downloaded and {sp_count} were removed due to being unnamed species\n\n")
+    elif status == 0: 
+        logger.info(f"{count} genomes of {genus} were downloaded\n\n")
+    
+    
+        
+    return status
+
+# Checking if the download worked 
+def download_checker(outdir, domain, logger, genus, sp_ignore): 
+    try:
+        # Check if the directory exists, if it does not then nothing was downloaded so raise and catch the exception
+        if Path(f"{outdir}/refseq/{domain}").is_dir():
+            count = len(list(Path(f"{outdir}/refseq/{domain}").glob("*/*.fna.gz"))) # count the number of genomes downloaded
+            dir_count = len(list(Path(f"{outdir}/refseq/{domain}").glob("*"))) # count the number of genomes that should have been downloaded based on dir count
             
             try:
-                if count != dir_count:
+                if count != dir_count: # check if expected and actual download count match and if not raise error
                     raise FileNotFoundError()
             except FileNotFoundError as err:
                 logger.error(str(err), exc_info = True)
-                logger.error(f"{genus} is a real genus but some (or no) genomes were not downloaded")
-                return 1
-            if sp_ignore:
-                sp_count = sp_remove(outdir, domain)
-                logger.info(f"{count} genomes of {genus} were downloaded and {sp_count} were removed due to being unnamed species\n\n")
-            else: 
-                logger.info(f"{count} genomes of {genus} were downloaded\n\n")
-            return 0
+                logger.error(f"{genus} is a real genus but some genomes were not downloaded, please repeat and hopefully this wont happen again ")
+                return 1, count
+            return 0, count
         
         else:
             raise NotADirectoryError()
     except NotADirectoryError as err:
         logger.error(str(err), exc_info = True)
         logger.error(f"Download failed because {genus} is invalid or there are no records of the requested type in NCBI")
-        return 1
-    return 0
+        return 1, count
+    return 0, count
 
 def sp_remove(outdir, domain):
     sp_count = 0
